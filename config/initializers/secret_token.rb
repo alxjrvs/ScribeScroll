5e34a1ed3c6b8f16e689119c41c0a25bb9013283
alxjrvs <alxjrvs@gmail.com>
2012-10-24 20:13:36 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScribeScroll::Application.config.secret_token = '5bcbb5da7f60faa9bee024b870a44e7a663bb706163d3a1479e00dce21dfdca625aea5ef7ff6ba726bac9e0b0e43da2e2e716d4482b11a56de1d75fc450c68ba'
