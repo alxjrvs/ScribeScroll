b933733a3d43cd29b8e9efc3ed25eec4359ed0d7
alxjrvs <alxjrvs@gmail.com>
2012-10-25 17:05:21 -0400

    reorganized Game module

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,15 +2,6 @@ module Game
 
   STATS = {str: {name: "strength", short_name: "STR"}, dex: {name: "dexterity", short_name: "DEX"}, con: {name: "constitution", short_name: "CON"}, wis: {name: "wisdom", short_name: "WIS"}, int: {name: "intellect", short_name: "INT"}, cha: {name: "charistma", short_name: "CHA"}}
 
-
-  def mod(attributes)
-    @stats = attributes
-  end
-
-  def stats
-    @stats
-  end
-
   ROLES = ["fighter", "wizard"]
   SKILLS = {:acrobatics => {:name => "Acrobatics", :stat => STATS[:dex]},
   #"Fly",
@@ -40,5 +31,16 @@ module Game
 #"Profession"
 
 }
+
+
+#Stats issues
+  def mod(attributes)
+    @stats = attributes
+  end
+
+  def stats
+    @stats
+  end
+
 
 end
