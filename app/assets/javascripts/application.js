c9a4ef5c49903553489769c775f5c4e35ef60177
alxjrvs <alxjrvs@gmail.com>
2012-11-10 16:43:23 -0500

    implemented point-buy javascript applications

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,5 +14,78 @@
 //= require jquery_ujs
 //= require twitter/bootstrap
 //= require_tree .
-
+function log(msg) {
+    setTimeout(function() {
+        throw new Error(msg);
+    }, 0);
+}
 var PointBuy = 20;
+var statHash = {};
+statHash[7] = 4;
+statHash[8] = 2;
+statHash[9] = 1;
+statHash[10] = 0;
+statHash[11] = -1;
+statHash[12] = -2;
+statHash[13] = -3;
+statHash[14] = -5;
+statHash[15] = -7;
+statHash[16] = -10;
+statHash[17] = -13;
+statHash[18] = -17;
+
+function totalCalc (oldval, newval){
+  var total = $('h1.total')
+  var submit = $('input#stats_submit')
+  PointBuy = PointBuy - statHash[parseInt(oldval)]
+  PointBuy = PointBuy + statHash[parseInt(newval)]
+  if (PointBuy < 0)
+    {
+      total.css('color', 'red')
+      submit.attr('disabled', 'disabled')
+    }
+  else
+    {
+      total.css('color', '#333')
+      submit.attr('disabled', false)
+    }
+  total.text(PointBuy)
+};
+
+$('.inc_button').click(function() {
+  $(this).parent().find('.desc_button').attr('disabled', false)
+  var input = $(this).parent().find('input')
+  var oldVal = input.val()
+  if (oldVal == 18)
+    { return false }
+  else
+  {
+  var newVal = input.val(parseInt(oldVal) + 1).val()
+  input.val(newVal)
+  totalCalc(oldVal, newVal)
+
+  if (newVal == 18)
+    {
+    $(this).attr("disabled", "disabled");
+    }
+  }
+});
+
+$('.desc_button').click(function() {
+  $(this).parent().find('.inc_button').attr('disabled', false)
+  var input = $(this).parent().find('input')
+  var oldVal = input.val()
+  if (oldVal == 7)
+    { return false }
+  else
+  {
+  var newVal = input.val(parseInt(oldVal) - 1).val()
+  input.val(newVal)
+  totalCalc(oldVal, newVal)
+  if (newVal == 7)
+    {
+    $(this).attr("disabled", "disabled");
+
+    }
+  }
+});
